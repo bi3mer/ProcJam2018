54f7dc658e74a0637c9b6a4fc10c15fd4cc35b0d
TStavrop <Trevor.Stavropoulos@gmail.com>
2018-10-23 10:49:02 -0700

    Simple updates to the spawning of shots and ripping a player control script from a unity tutorial.  Adding in some simple images to use as well.

--- Assets/Scripts/Player/PlayerAttacking.cs
+++ Assets/Scripts/Player/PlayerAttacking.cs
@@ -7,6 +7,10 @@ public class PlayerAttacking : MonoBehaviour
     [SerializeField]
     private Shot shotPrefab;
 
+    //The player is a 1 unit diameter circle.  So he ends at 0.5 units away from center.
+    //Adding 0.1 unit to spawn the shot just outside him.
+    private const float shotSpawnDisplacement = 0.6f;
+
     private void Awake()
     {
         Assert.IsNotNull(shotPrefab);
@@ -16,12 +20,22 @@ public class PlayerAttacking : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = transform.position.z;
+
+            Vector3 shotDirection = (mousePosition - transform.position).normalized;
+
+            if (shotDirection.x == 0f && shotDirection.y == 0f)
+            {
+                //Edge case where they clicked right in the middle of the player
+                shotDirection = Random.insideUnitCircle;
+            }
+
             GameObject shot = Instantiate(
-                shotPrefab.gameObject, 
-                transform.position, 
+                shotPrefab.gameObject,
+                transform.position + shotDirection * shotSpawnDisplacement,
                 Quaternion.identity);
-            shot.transform.LookAt(mousePosition);
+            shot.transform.forward = shotDirection;
         }
     }
 }
