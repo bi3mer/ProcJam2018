629392c2e156aab9487ac2f157ecbc09541e6910
Colan Biemer <colan@comcast.net>
2018-10-25 21:37:47 -0700

    pause menu

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused
+    {
+        get;
+        private set;
+    }
+
+    [SerializeField]
+    private GameObject playerHealth;
+
+    [SerializeField]
+    private Button resumeButton;
+
+    private void Awake()
+    {
+        Paused = false;
+        Assert.IsNotNull(playerHealth);
+        Assert.IsNotNull(resumeButton);
+    }
+
+    private void Start()
+    {
+        resumeButton.onClick.AddListener(UnPauseGame);
+        resumeButton.gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        resumeButton.onClick.RemoveListener(UnPauseGame);
+    }
+
+    private void Update()
+    {
+        Debug.Log("hello world");
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                UnPauseGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        Paused = true;
+        playerHealth.gameObject.SetActive(false);
+        resumeButton.gameObject.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void UnPauseGame()
+    {
+        Paused = false;
+        playerHealth.gameObject.SetActive(true);
+        resumeButton.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
